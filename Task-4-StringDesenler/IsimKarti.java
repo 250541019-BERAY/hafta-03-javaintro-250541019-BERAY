bee682c1ce3434c8dc5d025ab91da74c5de20fbe
250541019-BERAY <250541019@firat.edu.tr>
2025-10-23 20:28:18 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,14 @@
+public class isimkartı {
+    public static void main(String[] args) {
+        String ad = "Beray";
+        String soyad = "Akar";
+        String ogrenciNo = "250541019";
+        String bolum = "Yazilim Mühendisliği";
+
+        System.out.println("======ISIM KARTI======");
+        System.out.println("Ad        : " + ad);
+        System.out.println("Soyad     : " + soyad);
+        System.out.println("Öğrenci No: " + ogrenciNo);
+        System.out.println("Bölüm     : " + bolum);
+    }
+}
