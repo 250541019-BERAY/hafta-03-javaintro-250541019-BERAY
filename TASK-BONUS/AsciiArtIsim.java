879c6402df7fb458c6ea63f048f8a0d844d4c154
250541019-BERAY <250541019@firat.edu.tr>
2025-10-23 22:19:20 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,11 @@
+public class asciiisim {
+    public static void main(String[] args) {
+        System.out.println("Beray Beray Beray Beray");
+        System.out.println("Beray   Beray");
+        System.out.println("Beray   Beray");
+        System.out.println("Beray Beray Beray");
+        System.out.println("Beray");
+        System.out.println("Beray");
+        System.out.println("Beray");
+    }
+}
