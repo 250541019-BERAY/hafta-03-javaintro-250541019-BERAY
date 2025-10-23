8b935bd8297ccd62eb21a876af50a8394f56d725
250541019-BERAY <250541019@firat.edu.tr>
2025-10-23 21:07:19 +0300

    Update JavaDeseni.java

--- Task-1-HosgeldinMesajlari/JavaDeseni.java
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -1,12 +1,12 @@
-public class beskeretekrar {
+
+    public class javadesen {
  public static void main(String[] args) {
-
-    System.out.println("Java'ya hosgeldiniz");
-    System.out.println("Java'ya hosgeldiniz");
-    System.out.println("Java'ya hosgeldiniz");
-    System.out.println("Java'ya hosgeldiniz");
-    System.out.println("Java'ya hosgeldiniz");
-
-
-   }
+ System.out.println( "J A V V A ");
+ System.out.println(" J A A V V A A ") ;
+ System.out.println(" J  J  AAAAA V V AAAAA");
+ System.out.println(" J J A A V A A");
+ 
+    
 }
+}
+
