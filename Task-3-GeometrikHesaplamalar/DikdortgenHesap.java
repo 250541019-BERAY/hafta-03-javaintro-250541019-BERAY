e6b400579129ea76c597e67e5001bd0a4ce18ff5
250541019-BERAY <250541019@firat.edu.tr>
2025-10-23 20:19:51 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,15 @@
+public class dikdortgenalan { 
+    public static void main(String[] args) {
+      
+        double en = 4.5;
+        double boy = 7.9;
+
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.println("Dikdörtgenin eni: " + en);
+        System.out.println("Dikdörtgenin boyu: " + boy);
+        System.out.println("Dikdörtgenin alani: " + alan);
+        System.out.println("Dikdörtgenin cevresi: " + cevre);
+    }
+}
